58dbb36a3ac5164cb27e65f0d1a02437aed042f3
RS <RS@LAPTOP-VCPGANQJ>
2024-03-01 14:40:25 -0600

    Acitvidad 3_ Usuarios, autenticacion y estrategias

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import SpeciesRouter from './species.route'
+import UserRouter from './user.route'
+import AuthRouter from './auth.route'
 
 const routerApi = (app) => {
   const router = express.Router()
   app.use('/api/v1', router)
   router.use('/species', SpeciesRouter)
+  router.use('/users', UserRouter)
+  router.use('/auth', AuthRouter)
 }
 
 export default routerApi
